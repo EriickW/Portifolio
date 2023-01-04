16d4fbebf81f45cfe3bdec3747672f34cced1afd
Erick Willian <werick631@gmail.com>
2023-01-04 20:15:49 -0300

    Remake

--- src/script/main.js
+++
@@ -1,7 +0,0 @@
-const trocaTema = document.querySelector("#trocaTema");
-const body = document.querySelector("body")
-
-trocaTema.addEventListener("click",()=>{
-    body.classList.toggle("light-mode")
-    body.classList.toggle("dark-mode")
-})
